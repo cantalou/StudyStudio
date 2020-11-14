9ff03868f79f78913cfb9e545a1414d4e221eb4f
linzhiwei1@4399.inc <Linzhiwei1>
2020-11-14 11:12:33 +0800

    1. init project

--- app/src/main/java/com/wy/studystudio/ui/task/TaskFragment.kt
+++ app/src/main/java/com/wy/studystudio/ui/task/TaskFragment.kt
@@ -1,4 +1,4 @@
-package com.wy.studystudio.ui.dashboard
+package com.wy.studystudio.ui.task
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,19 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.wy.studystudio.R
 
-class DashboardFragment : Fragment() {
+class TaskFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
+    private lateinit var taskViewModel: TaskViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-                ViewModelProvider(this).get(DashboardViewModel::class.java)
+        taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
         val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+        taskViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/com/wy/studystudio/ui/task/TaskViewModel.kt
+++ app/src/main/java/com/wy/studystudio/ui/task/TaskViewModel.kt
@@ -1,10 +1,10 @@
-package com.wy.studystudio.ui.dashboard
+package com.wy.studystudio.ui.task
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class TaskViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
