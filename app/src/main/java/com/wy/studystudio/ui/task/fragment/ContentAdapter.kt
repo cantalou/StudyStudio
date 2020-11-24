b249b5478ad9307d90173eb1800c010ce7c71835
linzhiwei1@4399.inc <Linzhiwei1>
2020-11-24 20:10:14 +0800

    1. init project

--- app/src/main/java/com/wy/studystudio/ui/task/fragment/ContentAdapter.kt
+++ app/src/main/java/com/wy/studystudio/ui/task/fragment/ContentAdapter.kt
@@ -5,11 +5,12 @@ import android.view.View
 import com.bumptech.glide.Glide
 import com.wy.studystudio.R
 import com.wy.studystudio.databinding.ItemContentImageBinding
-import com.wy.studystudio.databinding.ItemContentTextBinding
+import com.wy.studystudio.extension.gvm
 import com.wy.studystudio.extension.startFragment
 import com.wy.studystudio.ui.common.adapter.BaseAdapter
 import com.wy.studystudio.ui.common.adapter.DBViewHolder
 import com.wy.studystudio.ui.task.model.Content
+import com.wy.studystudio.ui.task.vm.TaskViewModel
 
 /**
  *
@@ -20,6 +21,8 @@ import com.wy.studystudio.ui.task.model.Content
  * Copyright (c) 2020年, WY CO.ltd. All Rights Reserved.
  */
 class ContentAdapter : BaseAdapter<Content>() {
+
+    var corner: Int = 0
 
     override fun layoutId(): Array<Int> {
         return arrayOf(R.layout.item_content_text, R.layout.item_content_image, R.layout.item_content_image)
@@ -33,13 +36,25 @@ class ContentAdapter : BaseAdapter<Content>() {
         view.context.startFragment(EditContentFragment::class.java, 0, Bundle().apply { putParcelable("content", content) })
     }
 
+    fun delete(position: Int) {
+        val item = data.removeAt(position)
+        notifyDataSetChanged()
+        val taskVM = gvm(TaskViewModel::class.java)
+        val task = taskVM.get(item.taskId)
+        task.contents.removeAt(position)
+    }
+
     override fun onBindViewHolder(holder: DBViewHolder, position: Int) {
         super.onBindViewHolder(holder, position)
-        when(data[position].type){
+        if (corner == 0) {
+            corner = context.resources.getDimensionPixelOffset(R.dimen.corner)
+        }
+        when (data[position].type) {
             Content.TYPE_TEXT -> {
             }
             Content.TYPE_IMAGE -> {
-                Glide.with(holder.itemView).load(data[position].content).into((holder.vdb as ItemContentImageBinding).image)
+                Glide.with(holder.itemView).load(data[position].content)
+                    .into((holder.vdb as ItemContentImageBinding).image)
             }
         }
     }
